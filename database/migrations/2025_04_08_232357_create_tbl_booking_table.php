5dfbf24c5ab188bd8d87f1e51d6f782d05a66b78
Dang Tu <anhtuhanam1@gmail.com>
2025-04-08 23:26:55 +0700

    update

--- database/migrations/2025_04_08_232357_create_tbl_booking_table.php
+++ database/migrations/2025_04_08_232357_create_tbl_booking_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->unsignedBigInteger('userId'); // Sử dụng unsignedBigInteger để khóa ngoại
             $table->foreign('userId')->references('userId')->on('tbl_users'); // Khóa ngoại đến bảng tbl_tours
             $table->foreign('tourId')->references('tourId')->on('tbl_tours')->onDelete('cascade'); // Khóa ngoại đến bảng tbl_tours
-            $table->enum('bookingStatus', ['y', 'f', 'c','b'])->default('f');
+            $table->string('bookingStatus', 10)->default('f')->change();
             $table->integer('numChildren')->default(0);
             $table->decimal('totalPrice', 10, 2)->default(0);
             $table->integer('numAdults')->default(0); // Thêm cột 'bookingStatus'
