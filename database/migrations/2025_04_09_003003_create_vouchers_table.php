4f096709e6ed299fe186e7e9d9e41927c385c80f
Dang Tu <anhtuhanam1@gmail.com>
2025-04-14 17:01:26 +0700

    update

--- database/migrations/2025_04_09_003003_create_vouchers_table.php
+++ database/migrations/2025_04_09_003003_create_vouchers_table.php
@@ -12,9 +12,9 @@ class CreateVouchersTable extends Migration
         Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique(); // Mã voucher
-            $table->decimal('discount', 8, 2); // Giá trị giảm giá
+            $table->decimal('discount', 15, 2); // Giá trị giảm giá
             $table->enum('discount_type', ['percent', 'fixed']); // Loại giảm giá: phần trăm hoặc cố định
-            $table->decimal('min_order_value', 8, 2)->nullable(); // Giá trị đơn hàng tối thiểu
+            $table->decimal('min_order_value', 15, 2)->nullable(); // Giá trị đơn hàng tối thiểu
             $table->integer('max_usage')->default(1); // Số lần sử dụng tối đa
             $table->integer('used_count')->default(0); // Số lần đã sử dụng
             $table->dateTime('start_date'); // Ngày bắt đầu hiệu lực
